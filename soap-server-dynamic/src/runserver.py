e0ece2a788a998c95b8baa1e899ceec64aff7c8b
exengstfeld <exengstfeld@gmail.com>
2017-06-29 13:36:45 -0300

    Update runserver.py
    Fixed imports

--- soap-server-dynamic/src/runserver.py
+++ soap-server-dynamic/src/runserver.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
-# -*- coding: utf-8 -*-         
-#!/usr/bin/python
-# -*- coding: ascii -*-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#!/usr/bin/python
 # -*- coding: latin-1 -*-
-#!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
 
 from jinja2 import Template, Environment, FileSystemLoader
-from flask import Flask, current_app, jsonify, render_template, request, url_for, redirect, flash, session, make_response
+from flask import Flask, current_app, jsonify, render_template, request, url_for, session, make_response
 from flask_cors import cross_origin
 import json
 from functools import wraps
 import os
-import uuid
 import json
-from pymongo import MongoClient
 from flask.ext.spyne import Spyne
 from spyne.protocol.soap import Soap11
 from spyne.model.primitive import Unicode, Integer
 from spyne.model.complex import Iterable
 import httplib2 as http
-import sys
 from pysimplesoap.client import SoapClient, SoapFault
 from pysimplesoap.simplexml import SimpleXMLElement
-from base64 import b64decode
-from bson.json_util import dumps
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
@@ -46,9 +32,6 @@ except Exception, e:
     print "Lo sentimos, pero no se encuentra la referencia a la variable de ambiente SOAPSERVER_SETTINGS la cual debe estar definida y poseer el archivo de configuracion correspondiente"
     sys.exit(-1)
 
-reload(sys)
-sys.setdefaultencoding("utf-8")
-
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('runserver.py')
 soap_server_code = template.render(external_services=app.config["EXTERNAL_SERVICES"], services=app.config["SERVICES"], namespace=app.config["NAMESPACE"],soap_location=app.config["SERVICE_LOCATION"],soap_validator=app.config["VALIDATOR"])
@@ -56,7 +39,7 @@ exec(soap_server_code)
 
 @app.route("/") 
 def index(): 
-		return "esto parece que funca" 
+	return "esto parece que funca" 
 
 if __name__ == '__main__':
 	app.debug=False
